88b27f8a9b3d8eca8ec46223cd0b72a4141469b1
avgtrash <codysoloveoff@gmail.com>
2025-11-14 15:00:28 -0800

    Login functionality for admin. Added getters for admin.

--- headers/admin.h
+++ headers/admin.h
@@ -12,17 +12,23 @@ class AccountService;
 class Admin : public Account{
 public:
     // Parameterized  (changed username adminID same as UML)
-    Admin(const std::string &adminID, int password, AccountService &accountService);
+    Admin(const std::string &adminID, string password, AccountService &accountService);
 
     // Create an account, pass in a customer object
     void createAccount(const Customer &customer);
 
     // View all accounts (returns a list of all accounts)
     void viewAllAccounts();
+
+    //getters
+    const std::string &getAdminID() const;
+    const std::string &getPassword() const;
+
+    bool login() const;
     
 private:
     std::string adminID;
-    int password;
+    std::string password;
     AccountService &accountService;
 };
 
--- programfiles/admin.cpp
+++ programfiles/admin.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // Parameterized constructor
-Admin::Admin(const std::string &adminIDValue, int passwordValue, AccountService &service)
+Admin::Admin(const std::string &adminIDValue, string passwordValue, AccountService &service)
     : Account(),
       adminID(adminIDValue),
       password(passwordValue),
@@ -16,7 +16,37 @@ void Admin::createAccount(const Customer &customer) {
     accountService.createAccount(customer);
 }
 
+//Login
+bool Admin::login() const
+{
+    bool result = false;
+    string name;
+    string password;
+    while(true)
+    {
+        cout << "Please enter your username: ";
+        cin >> name;
+        cout << "Please enter your password: ";
+        cin >> password;
+
+        if(name == getAdminID() && password == getPassword())
+        {
+            result = true;
+            break;
+        }
+        else
+        {
+            cout << "Your username or password is incorrect, please try again.";
+        };
+    }    
+    return result;
+}
+
 // View all accounts (returns a list of all accounts)
 void Admin::viewAllAccounts() {
     accountService.viewAllAccounts();
 }
+
+//getters
+const std::string &Admin::getAdminID() const {return adminID;}
+const std::string &Admin::getPassword() const {return password;}
