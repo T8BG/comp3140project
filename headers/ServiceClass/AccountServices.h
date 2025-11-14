81f91c81cca6708e0522164b351907a3debe5a91
avgtrash <codysoloveoff@gmail.com>
2025-11-14 15:55:56 -0800

    Prepped function headers for admin control.

--- headers/ServiceClass/AccountServices.h
+++ headers/ServiceClass/AccountServices.h
@@ -9,12 +9,18 @@ public:
     // Constructor (passes in the repostory object)
     AccountService(AccountRepository &repository);
 
-    // createAccount, generateAccountNumber methods 
+    // createAccount, generateAccountNumber, deleteAccount methods 
     void createAccount(const Customer &customer);
     int generateAccountNumber() const;
+    void deleteAccount(Customer &customer);
 
     // View all accounts (returns a list of all accounts)
     void viewAllAccounts();
+
+    //Freeze and unfreeze methods
+    void freezeAccount(const std::string &customerID);
+
+    void unfreezeAccount(const std::string &customerID);
 
 private:
     AccountRepository &accountRepository;
