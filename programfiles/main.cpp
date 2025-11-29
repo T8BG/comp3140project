2af55e36df220206115b93bd53f05091bf6513b7
avgtrash <codysoloveoff@gmail.com>
2025-11-29 03:53:12 -0800

    Began implementation of main menu.

--- programfiles/main.cpp
+++ programfiles/main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 #include "../headers/ServiceClass/AuthenticationService.h"
 
 int main() {
+    //TESTING FOR STAGE 4
     // Create repository and servidce objects
     AccountRepository accountRepository;
     AccountService accountService(accountRepository);
@@ -32,6 +33,132 @@ int main() {
     std::cout << std::endl << "=== Admin Login ===" << std::endl;
     bool loginResult = authService.authenticateAdmin(admin);
     cout << "Admin login result: " << loginResult << endl;
+
+    //DEFINE VARIABLES
+    int Acctnumber;
+    string username;
+    string password;
+
+    int input;
     
+    //USER SELECTS WHICH ACCOUNT THEY WANT TO ACCESS
+    cout << "Which type of account do you request?";
+    cout << "1. Customer";
+    cout << "2. Admin";
+    while(true)
+    {
+        cin >> Acctnumber;
+        switch (Acctnumber)
+        {
+            case 1:
+            break;
+
+            case 2:
+            break;
+
+            default:
+            cout << "Please enter a '1' or a '2'";
+        }
+    }
+
+    //USER ENTERS THEIR USERNAME AND PASSWORD
+    cout << "Please enter your username";
+    cin >> username;
+    cout << "Please enter your password";
+    cin >> password;
+
+    //USER SELECTS WHAT TASK THEY WISH TO DO
+    // 1 = USER
+    // 2 = ADMIN
+    cout << "Please select operation";
+    if(Acctnumber == 1)
+    {
+        cout << "1. Deposit";
+        cout << "2. Withdraw";
+        cout << "3. Transfer";
+        cout << "4. View History";
+        cout << "5. Logout";
+
+    }
+    else if(Acctnumber ==2)
+    {
+        cout << "1. Create Account";
+        cout << "2. Delete Account";
+        cout << "3. Freeze Account";
+        cout << "4. Unfreeze Account";
+        cout << "5. Find Account Details";
+        cout << "6. Logout";
+    }
+    cout << "Please enter command.";
+    while(true)
+    {
+        cin >> input;
+        switch (input){
+            case 1:
+            if(Acctnumber == 1)
+            {
+
+            }
+            else if(Acctnumber == 2)
+            {
+
+            }
+
+            case 2:
+            if(Acctnumber == 1)
+            {
+
+            }
+            else if(Acctnumber == 2)
+            {
+                
+            }
+
+            case 3:
+            if(Acctnumber == 1)
+            {
+
+            }
+            else if(Acctnumber == 2)
+            {
+                
+            }
+
+            case 4:
+            if(Acctnumber == 1)
+            {
+
+            }
+            else if(Acctnumber == 2)
+            {
+                
+            }
+
+            case 5:
+            if(Acctnumber == 1)
+            {
+
+            }
+            else if(Acctnumber == 2)
+            {
+                
+            }
+
+            case 6:
+            if(Acctnumber == 1)
+            {
+                cout << "Please enter a valid value.";
+            }
+            else if(Acctnumber == 2)
+            {
+                
+            }
+
+            default:
+            cout << "Please enter a valid value.";
+        }
+        break;
+    }
+
     return 0;
 }
